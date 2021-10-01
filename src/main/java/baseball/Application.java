ffaf1283a294c422a451055a7f6ffb3d96ec479d
sanghyuk han <jshans40@gmail.com>
2021-10-01 09:26:49 +0900

    feat : 야구게임 사용자 입력 기능 추가
    입력한 글자가 3개가 맞는지 체크
    입력한 글자가 모두 정수가 맞는지 체크
    1~9까지의 범위의 숫자를 입력한건지 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,77 @@
 package baseball;
 
+import nextstep.utils.Console;
+
 public class Application {
+    private static boolean isGameFinish = false;
+
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+        loop();
     }
+
+    private static void loop() {
+        while(!isGameFinish) {
+            userInput();
+        }
+    }
+
+    private static void userInput() {
+        boolean isCorrectUserInput = false;
+
+        while (!isCorrectUserInput) {
+            String userInput = Console.readLine();
+            isCorrectUserInput = validateUserInput(userInput);
+        }
+    }
+
+    private static boolean validateUserInput(String userInput) {
+        if (userInput.length() != 3) {
+            System.out.println("입력한 숫자를 다시 한번 확인해주세요.");
+            return false;
+        }
+
+        if (!validateUserInputString(userInput)) {
+            System.out.println("입력한 숫자를 다시 한번 확인해주세요.");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean validateUserInputString(String userInput) {
+        if (!isIntegerWithRange(userInput)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isIntegerWithRange(String userInput) {
+        if (!isInteger(userInput.charAt(0)) || !isInteger(userInput.charAt(1)) || !isInteger(userInput.charAt(2))) {
+            return false;
+        }
+
+        if (Integer.parseInt(String.valueOf(userInput.charAt(0))) <= 0 || Integer.parseInt(String.valueOf(userInput.charAt(0))) > 9) {
+            return false;
+        }
+
+        if (Integer.parseInt(String.valueOf(userInput.charAt(1))) <= 0 || Integer.parseInt(String.valueOf(userInput.charAt(1))) > 9) {
+            return false;
+        }
+
+        if (Integer.parseInt(String.valueOf(userInput.charAt(2))) <= 0 || Integer.parseInt(String.valueOf(userInput.charAt(2))) > 9) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isInteger(char charUserInput) {
+        try {
+            Integer.parseInt(String.valueOf(charUserInput));
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
 }
